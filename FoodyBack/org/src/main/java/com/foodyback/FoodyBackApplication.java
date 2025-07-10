176a230e2faf7c7f25ec000fd6f8e201a98057d1
massinabassene <massinasylvanusbassene@esp.sn>
2025-07-10 02:01:59 +0000

    update

--- FoodyBack/org/src/main/java/com/foodyback/FoodyBackApplication.java
+++ FoodyBack/org/src/main/java/com/foodyback/FoodyBackApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodyBackApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(FoodybackApplication.class, args);
+		SpringApplication.run(FoodyBackApplication.class, args);
 	}
 }
